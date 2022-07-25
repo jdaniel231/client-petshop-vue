9ab4d8cc7e0e172716c0f265b49b1f061e8724fb
hammer200911 <jdaniel231@gmail.com>
2022-07-25 14:12:18 -0300

    axios customers

--- src/components/customers/List.vue
+++ src/components/customers/List.vue
@@ -1,32 +1,37 @@
 <template>
-  <v-data-table
-    :headers="headers"
-    :items="desserts"
-    sort-by="calories"
-    class="elevation-1"
-    :search="search"
-  >
-    <template v-slot:top>
-      <v-toolbar
-        flat
-      >
-        <v-spacer></v-spacer>
-
-        <v-text-field
-          v-model="search"
-          append-icon="mdi-magnify"
-          label="Pesquisar"
-          single-line
-          hide-details
-        ></v-text-field>
-        </v-toolbar>
-        </template>
-  </v-data-table>
+  <v-simple-table>
+    <template v-slot:default>
+      <thead>
+        <tr>
+          <th class="text-left">Nome</th>
+          <th class="text-left">Telefone</th>
+          <th class="text-center"></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="item in customers" :key="item.id">
+          <td class="text-left">{{ item.name }}</td>
+          <td class="text-left">{{ item.phone }}</td>
+          <td class="text-center">
+            <v-btn small color="primary" class="mx-3">
+              <v-icon> mdi-pencil </v-icon>
+            </v-btn>
+             <v-btn small color="primary" class="mx-3">
+              <v-icon> mdi-delete </v-icon>
+            </v-btn>
+          </td>
+        </tr>
+      </tbody>
+    </template>
+  </v-simple-table>
 </template>
 
 <script>
 export default {
   name: 'ListCustomers',
+  props: {
+    customers: Array,
+  },
   methods: {
      newClient() {
       this.$router.push("/customers/new");
